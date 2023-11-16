a08a7094966998a3c75d147bb50ede15486e9053
Kingsley <louis.officialz@gmail.com>
2023-11-16 05:40:31 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -2,10 +2,12 @@
 
 /**
  * main - This is the main function.
- * _leomous() - Function to display prompt.
+ *
+ * _leomous() - Function to display the prompt.
  * interpret_format() - Function prompts the user to enter a format
  * and reads it from stdin.
- * perform_format() - Function executes the command.
+ * _execute() - Function executes the command.
+ *
  * Return: 0 Always (success).
  */
 int main(void)
@@ -16,7 +18,7 @@ int main(void)
 	{
 		_leomous();
 		interpret_format(format, sizeof(format));
-		perform_format(format);
+		_execute(format, NULL, NULL);
 	}
 
 	return (0);
